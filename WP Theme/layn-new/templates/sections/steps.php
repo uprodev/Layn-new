9c0d03179ccb2293216eaa464a3b08834adcafc5
Stanislav-pr <77438513+Stanislav-pr@users.noreply.github.com>
2025-01-08 14:58:32 +0100

    span

--- WP Theme/layn-new/templates/sections/steps.php
+++ WP Theme/layn-new/templates/sections/steps.php
@@ -29,15 +29,17 @@ $link = get_sub_field('link');
                         $link_title = $link['title'];
                         $link_target = $link['target'] ? $link['target'] : '_self';?>
                         <div class="buttons">
-                            <a href="<?= esc_url($link_url); ?>" target="<?= esc_attr($link_target); ?>" class="btn btn-outline-white btn-anchor">
-                                <?= esc_html($link_title); ?>
-                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <g>
-                                        <path d="M1.85862 0.443138L0.882812 1.41895L14.1693 14.7055L15.1452 13.7297L1.85862 0.443138Z" fill="white" />
-                                        <path d="M17.9998 16.1804V3.40039H16.6198V16.1804H3.83984V17.5604H16.6198H17.9998V16.1804Z" fill="white" />
-                                    </g>
-                                </svg>
-                            </a>
+                            <span>
+                                <a href="<?= esc_url($link_url); ?>" target="<?= esc_attr($link_target); ?>" class="btn btn-outline-white btn-anchor">
+                                    <?= esc_html($link_title); ?>
+                                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <g>
+                                            <path d="M1.85862 0.443138L0.882812 1.41895L14.1693 14.7055L15.1452 13.7297L1.85862 0.443138Z" fill="white" />
+                                            <path d="M17.9998 16.1804V3.40039H16.6198V16.1804H3.83984V17.5604H16.6198H17.9998V16.1804Z" fill="white" />
+                                        </g>
+                                    </svg>
+                                </a>
+                            </span>
                         </div>
                     <?php endif; ?>
                 </div>
