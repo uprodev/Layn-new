89c97a3caf7f7c186fa4a0427e7462295898ac66
Vyacheslav <perminov0903@gmail.com>
2025-03-04 17:23:55 +0300

    Theme update

--- WP Theme/layn-new/templates/sections/maps.php
+++ WP Theme/layn-new/templates/sections/maps.php
@@ -56,11 +56,25 @@ $locations = get_sub_field('locations');
                                         <?php if($loc['text']):?>
                                             <p><?= $loc['text'];?></p>
                                         <?php endif;?>
-                                        <?php if($loc['bottom_text']):?>
-                                            <div class="tab-footer">
+
+                                        <div class="tab-footer">
+
+                                            <?php if ($loc['link']): ?>
+                                                <div class="buttons">
+                                                    <a href="<?= $loc['link']['url'] ?>" class="btn btn-outline-dark btn-anchor"<?php if($loc['link']['target']) echo ' target="_blank"' ?>>
+                                                        <?= html_entity_decode($loc['link']['title']) ?>
+                                                        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                            <g><path d="M1.85862 0.443138L0.882812 1.41895L14.1693 14.7055L15.1452 13.7297L1.85862 0.443138Z" fill="white" /><path d="M17.9998 16.1804V3.40039H16.6198V16.1804H3.83984V17.5604H16.6198H17.9998V16.1804Z" fill="white" /></g>
+                                                        </svg>
+                                                    </a>
+                                                </div>
+                                            <?php endif ?>
+
+                                            <?php if($loc['bottom_text']):?>
                                                 <p><?= $loc['bottom_text'];?></p>
-                                            </div>
-                                        <?php endif;?>
+                                            <?php endif;?>
+                                        </div>
+                                        
                                     </div>
                                     <?php $m++; endforeach; $m = 1;?>
                                 </div>
